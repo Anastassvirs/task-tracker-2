508a0c674a63aceb35730878980b368696866487
Анастасия Свирская <foreducationYa@yandex.ru>
2022-08-20 21:26:37 +0300

    fix: main изменен на рабочий вариант

--- src/Main.java
+++ src/Main.java
@@ -18,24 +18,24 @@ public class Main {
                 LocalDateTime.of(2022, 1, 1, 0, 0));
         Integer taskNum1 = manager.addNewTask(newTask);
         newTask = new Task("taskie daski", "funny and nice", Status.NEW, (long) 20,
-                LocalDateTime.of(2022, 1, 1, 0, 0));
+                LocalDateTime.of(2022, 2, 1, 0, 0));
         Integer taskNum2 = manager.addNewTask(newTask);
         // Три подзадачи + эпик
         Epic newEpic = new Epic("ep1", "usual epic you know", Status.NEW, (long) 20,
-                LocalDateTime.of(2022, 1, 1, 0, 0));
+                LocalDateTime.of(2022, 3, 1, 0, 0));
         Integer epicTaskNum1 = manager.addNewEpic(newEpic);
         Subtask newSubtask = new Subtask("sub1", "little subbie", Status.NEW, (long) 20,
-                LocalDateTime.of(2022, 1, 1, 0, 0), epicTaskNum1);
+                LocalDateTime.of(2022, 4, 1, 0, 0), epicTaskNum1);
         Integer subtask1 = manager.addNewSubtask(newSubtask);
         newSubtask = new Subtask("sub2", "subsub", Status.NEW, (long) 20,
-                LocalDateTime.of(2022, 1, 1, 0, 0), epicTaskNum1);
+                LocalDateTime.of(2022, 5, 1, 0, 0), epicTaskNum1);
         Integer subtask2 = manager.addNewSubtask(newSubtask);
         newSubtask = new Subtask("sub3", "subsubsub", Status.NEW, (long) 20,
-                LocalDateTime.of(2022, 1, 1, 0, 0), epicTaskNum1);
+                LocalDateTime.of(2022, 6, 1, 0, 0), epicTaskNum1);
         Integer subtask3 = manager.addNewSubtask(newSubtask);
         // Пустой эпик
         newEpic = new Epic("ep2", "cool epic doesn't need subtasks", Status.NEW, (long) 20,
-                LocalDateTime.of(2022, 1, 1, 0, 0));
+                LocalDateTime.of(2022, 7, 1, 0, 0));
         Integer epicTaskNum2 = manager.addNewEpic(newEpic);
 
         System.out.println('\n' + "Список обычных задач: " + manager.getAllTasks());
